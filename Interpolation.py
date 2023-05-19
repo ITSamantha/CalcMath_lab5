58483adca1cfd1b15019fa10e459895f46e5df04
Diana <diana.saveleva.dos@gmail.com>
2023-05-19 12:36:39 +0300

    Initial commit

---
+++ Interpolation.py
@@ -0,0 +1,16 @@
+class Interpolation:
+
+    def __init__(self, x_array, y_array, x):
+        self.__x_array = x_array
+        self.__y_array = y_array
+        self.__x = x
+
+    def getX(self):
+        return self.__x
+
+    def getArrayX(self):
+        return self.__x_array
+
+    def getArrayY(self):
+        return self.__y_array
+
